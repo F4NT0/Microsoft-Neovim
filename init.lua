a7415c2e189883e42b88ec54e7737e4083d3c008
F4NT0 <fantolaboratorio@hotmail.com>
2023-09-03 18:27:17 -0300

    Exemplos de Keymaps

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
+-- opções de configuração do vim
 require "fanto.options"
+-- Keymaps básicos de uso
+require "fanto.keymaps"
--- lua/fanto/keymaps.lua
+++ lua/fanto/keymaps.lua
@@ -41,7 +41,7 @@ vim.g.maplocalleader = " "
 -- MELHOR NAVEGAÇÃO ENTRE TELAS
 --------------------------------
 keymap("n", "<C-h>h", ":split<CR>", opts) -- Ctrl + H + H Comando para abrir uma nova tela na horizontal = :split
-keymap("n", "<C-v>", ":vsplit<CR>", opts) -- Comando para abrir uma nova tela na vertical = :vsplit
+keymap("n", "<C-h>v", ":vsplit<CR>", opts) -- Comando para abrir uma nova tela na vertical = :vsplit
 keymap("n", "<C-h>", "<C-w>h", opts)      -- Ctrl + H move para a tela a esquerda
 keymap("n", "<C-j>", "<C-w>j", opts)      -- Ctrl + J move para a tela abaixo  
 keymap("n", "<C-k>", "<C-w>k", opts)      -- Ctrl + K move para a tela encima
@@ -56,4 +56,34 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)  -- Resize para a esque
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts) -- Resize para a direita
 
 
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
